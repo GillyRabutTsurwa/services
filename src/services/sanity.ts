61ec02e81b054abcb9aa73ce52cfa6bee43fe279
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-19 18:23:44 -0500

    installed & set-up @sanity/client

---
+++ src/services/sanity.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@sanity/client";
+
+const sanityClient = createClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_ENV,
+    apiVersion: process.env.SANITY_API_VERSION,
+    useCdn: false,
+});
+
+export default sanityClient;
